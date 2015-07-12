287d48d54f72d00a6738f5b88f5a52ffd7556749
eladzlot <eladzlot@gmail.com>
2015-07-12 19:09:23 +0300

    chore(init): first commit

---
+++ schemas/element.js
@@ -0,0 +1,27 @@
+var a = {
+  title: 'Basic element',
+  type:'object',
+  properties:{
+    inherit: {$ref:'#/definitions/inherit'},
+    data: {type:'object', options:{no_additional_properties:false}, description:'Sets up some local variables.'},
+    merge: {type:'array',uniqueItems: true,format:'table',items:{type:'string'}, description:'Attributes inheritance extends instead of overriding.'},
+    regenerateTemplate: {type:'boolean', description:'Regenerate templates each time they are encountered'}
+  },
+  defaultProperties: ['inherit'],
+  definitions: {
+    inherit: {
+      id: 'inherit',
+      title: 'inherit',
+      type: 'object',
+      format:'grid',
+      properties: {
+        set: {type: 'string',description: 'Which set are we inheriting from.'},
+        type:{'enum':['random','exRandom','sequential','byData'], description:'The inheritance method.'},
+        seed:{type:'string'},
+        repeat:{type:'boolean'}
+      },
+      defaultProperties: ['set','type']
+
+    }
+  }
+}
